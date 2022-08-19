5a14c88a24516a66420861ac16f8e7bdd1c2e0a1
nonoaa <nonoaa1@naver.com>
2022-08-19 16:57:03 +0900

    changed main

--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -4,9 +4,9 @@ bool bsp(Point const a, Point const b, Point const c, Point const point);
 
 int main()
 {
-	Point a = Point(0.0f, 0.0f);
-	Point b = Point(10.0f, 0.0f);
-	Point c = Point(10.0f, 10.0f);
+	Point a(0.0f, 0.0f);
+	Point b(10.0f, 0.0f);
+	Point c(10.0f, 10.0f);
 
 	std::cout << bsp(a, b, c, Point(5.0f, 2.0f)) << std::endl;
 
